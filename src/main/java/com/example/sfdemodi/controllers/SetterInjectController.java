9b83faa7902f1bec1ab668fd5627687a94d3d13a
huytn1991 <tn.desperado@gmail.com>
2018-11-16 11:34:52 +0800

    Issue 3: Add setter dependency inject

---
+++ src/main/java/com/example/sfdemodi/controllers/SetterInjectController.java
@@ -0,0 +1,27 @@
+package com.example.sfdemodi.controllers;
+
+import com.example.sfdemodi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/setter")
+public class SetterInjectController {
+
+    private GreetingService greetService;
+
+    @Autowired
+    public void setGreetingService(GreetingService greetService) {
+        this.greetService = greetService;
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String saidHello() {
+        return greetService.saidHello();
+    }
+
+
+}

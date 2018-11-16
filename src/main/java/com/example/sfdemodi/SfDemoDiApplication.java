155bcb23356f0abcaa8fa5842e21e0ae463abb54
huytn1991 <tn.desperado@gmail.com>
2018-11-16 11:04:58 +0800

    Add initialized project.

---
+++ src/main/java/com/example/sfdemodi/SfDemoDiApplication.java
@@ -0,0 +1,12 @@
+package com.example.sfdemodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfDemoDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfDemoDiApplication.class, args);
+    }
+}
